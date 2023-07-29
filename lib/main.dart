fedbd9ccd741dd29ecdcd5cc1afa57912c9812b1
coderbaba <sattababakingup@gmail.com>
2023-07-29 13:31:38 +0530

    filterscreen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Furniture Shop UI',
+      title: 'Furniture Shop ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
